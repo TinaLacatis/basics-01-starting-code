2bb1b58a6b009e1ca9a59390f48a8121a61b56e0
Lacatis, Danuta Tina <danutatina.lacatis@iso-gruppe.com>
2023-10-29 16:29:06 +0100

    outputing row HTML with v-html

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       courseGoalA: "Finish the course and learn Vue!",
-      courseGoalB: "Master Vue and build amazing app!",
+      //   courseGoalB: "Master Vue and build amazing app!",
+      courseGoalB: "<h2>Master Vue and build amazing app!</h2>",
 
       vueLink: "https://vuejs.org/",
     };
