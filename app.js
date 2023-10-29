ae709ddaa4423830b340d4a22edb285089a36135
Lacatis, Danuta Tina <danutatina.lacatis@iso-gruppe.com>
2023-10-29 11:53:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoal: "Finish the course and learn Vue",
+      vueLink: "https://vuejs.org/",
+    };
+  },
+});
+methods: {
+}
+app.mount("#user-goal");
